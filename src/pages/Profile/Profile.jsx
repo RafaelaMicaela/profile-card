aaf4763ac932a97503d3534cd10532207615c849
Rafaela Medeiros <33203072+RafaelaMicaela@users.noreply.github.com>
2022-08-03 14:22:13 -0300

    Update Profile.jsx

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import bgcard from "../../assets/bg-pattern-card.svg";
 import imagevictor from "../../assets/image-victor.jpg";
 
 export default function Profile() {
-   var user = {
+   conts user = {
     nome: 'Victor Crest',
     idade: 26,
     localidade: 'London',
